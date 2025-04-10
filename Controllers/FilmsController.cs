4d916684b507c7a60c4e798766f7d5f627c853ed
Demonek555 <bartosz555-2000@o2.pl>
2025-04-10 10:17:27 +0200

    10.04.2025

--- Controllers/FilmsController.cs
+++ Controllers/FilmsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.DAL;
 using WebApplication1.Models;
@@ -8,10 +9,12 @@ namespace WebApplication1.Controllers
     public class FilmsController : Controller
     {
         FilmsContext db;
+        IWebHostEnvironment webHost;
 
-        public FilmsController(FilmsContext db)
+        public FilmsController(FilmsContext db, IWebHostEnvironment webHost)
         {
             this.db = db;
+            this.webHost = webHost;
         }
 
         public IActionResult AllFilms()
@@ -38,6 +41,25 @@ namespace WebApplication1.Controllers
                 db.Categories.Find(film.CategoryId);
             }
             return View(film);
+        }
+        [HttpGet]
+        public IActionResult AddFilm()
+        {
+            AddViewModel model = new AddViewModel();
+            model.AllCategories = db.Categories.ToList();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult AddFilm(AddViewModel model)
+        {
+            var posterFolderPath = Path.Combine(webHost.WebRootPath, "posters");
+            var uniquePosterName = model.Poster.FileName + "_" + Guid.NewGuid()+Path.GetExtension(model.Poster.FileName);
+            var filePath = Path.Combine(posterFolderPath, uniquePosterName);
+            model.Poster.CopyTo(new FileStream(filePath, FileMode.Create));
+            model.NewFilm.Poster = uniquePosterName;
+            db.Films.Add(model.NewFilm);
+            db.SaveChanges();
+            return RedirectToAction("AddFilm");
         }
     }
 }
---
+++ Models/AddViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class AddViewModel
+    {
+        public Film NewFilm { get; set; }
+        public IFormFile Poster { get; set; }
+        public List <Category> AllCategories { get; set; }
+    }
+}
